1bc254cb5567b2c067f55ceb78ab86f61bc877ac
phibuinek <phibuinek@gmail.com>
2024-05-28 08:30:27 +0700

    lỗi deploy thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,19 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import {thunk} from 'redux-thunk'
-import { authReducer } from './reducers/auth.reducer'
-import { homeVideosReducer, searchVideosReducer } from './reducers/videos.reducer'
-
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'; // Sử dụng xuất khẩu mặc định
+import { authReducer } from './reducers/auth.reducer';
+import { homeVideosReducer, searchVideosReducer } from './reducers/videos.reducer';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     homeVideos: homeVideosReducer,
     searchVideos: searchVideosReducer
-})
+});
+
 const store = createStore(
     rootReducer,
     {},
     composeWithDevTools(applyMiddleware(thunk))
-)
+);
 
-export default store
+export default store;
